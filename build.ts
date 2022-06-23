69e03c430b79b2de7c518c2e2d0ee34db1768f81
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-06-23 17:07:28 +0900

    Chore/typescript (#1)
    * Create package.json
    
    * Chore: install esbuild
    
    * Create .editorconfig
    
    * Add: build
    
    * Add: eslint
    
    * Add: prepublishOnly

---
+++ build.ts
@@ -0,0 +1,31 @@
+import { build } from 'esbuild';
+import { dtsPlugin } from 'esbuild-plugin-d.ts';
+import { rm } from 'fs/promises';
+const { dependencies, devDependencies, peerDependencies } = require('./package.json');
+
+rm('./lib', { recursive: true });
+
+const buildOptions = {
+  bundle: true,
+  entryPoints: ['./src/index.ts'],
+  external: [
+    ...Object.keys(dependencies ?? []),
+    ...Object.keys(devDependencies ?? []),
+    ...Object.keys(peerDependencies ?? [])
+  ],
+  minify: true,
+  sourcemap: false,
+};
+
+build({
+  ...buildOptions,
+  format: 'esm',
+  outfile: './lib/index.esm.js',
+});
+
+build({
+  ...buildOptions,
+  format: 'cjs',
+  outdir: './lib',
+  plugins: [dtsPlugin()]
+});
