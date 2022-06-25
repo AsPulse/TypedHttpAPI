9a9c857c21b33e946ad887d04de0b0de01ef015a
AsPulse <aspulse.x@gmail.com>
2022-06-25 15:30:03 +0900

    Add: migration-runTypes

--- src/interface/schema.ts
+++ src/interface/schema.ts
@@ -1,15 +1,14 @@
-import type { Type } from 'io-ts';
+import type { InternalRecord } from 'runtypes';
+import type { RuntypeBase } from 'runtypes/lib/runtype';
 import type { HttpRequestMethod } from './httpMethod';
 
+type FieldRuntypeBase = { [_: string]: RuntypeBase };
 
-export type APISchemaIO = {
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  request: Type<any>,
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  response: Type<any>
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyAPISchemaIO = APISchemaIO<any, any>;
+export type APISchemaIO<T extends FieldRuntypeBase, U extends FieldRuntypeBase> = {
+  request: InternalRecord<T, false, false>,
+  response: InternalRecord<U, false, false>
 };
 
 export type APIEndPoint =`${HttpRequestMethod} /${string}`;
@@ -18,7 +17,7 @@ export type APIEndPoint =`${HttpRequestMethod} /${string}`;
  * For each API endpoint, specify the request method, request interface and response interfaces.
  */
 export type APISchema = {
-  [key: APIEndPoint]: APISchemaIO,
+  [key: APIEndPoint]: AnyAPISchemaIO,
 };
 
 export const generateAPISchema = <T extends ReadonlyArray<APISchema>>(...input: T): T[0] => input[0];
