de3b4358884a613df1bbd0aff53b1d59a1f1dd3b
AsPulse <aspulse.x@gmail.com>
2022-06-26 11:25:10 +0900

    Add: shortages

--- src/entry/server.ts
+++ src/entry/server.ts
@@ -3,9 +3,12 @@ import type { APIImplements, APIImplement, APIExport } from '../interface/api';
 import { HttpAPIRequest, HttpAPIResponse } from '../interface/httpInterface';
 import { HTTP_REQUEST_METHODS } from '../interface/httpMethod';
 import type { APISchema, APIEndPoint } from '../interface/schema';
+import type { BetterObjectConstructor } from '../util/betterObjectConstrutor';
 import { detectDuplicate } from '../util/detectDuplicate';
 import { generateSummary } from '../util/formatSummary';
 import { parseEndPoint } from '../util/parseEndPoint';
+
+declare const Object: BetterObjectConstructor;
 
 export class TypedHttpAPIServer<APISchemaType extends APISchema, Raw = undefined> {
   private implementations: APIImplements<APISchemaType, Raw>[] = [];
@@ -31,12 +34,13 @@ export class TypedHttpAPIServer<APISchemaType extends APISchema, Raw = undefined
         return HttpAPIResponse.unpack(await v.processor(new HttpAPIRequest(request), payload));
       },
     }));
+    const shortage = Object.entries(this.schema).map(v => v[0]).filter(v => types.find(e => `${e.endpoint.method} ${e.endpoint.uri}` === v) === undefined);
     if(summary) generateSummary({
       apiCount: HTTP_REQUEST_METHODS.map(v => ({ method: v, count: types.filter(e => e.endpoint.method === v).length })),
       doublingEndpoints: detectDuplicate(types.map(v => `${v.endpoint.method} ${v.endpoint.uri}`)),
-      shortageEndpoints: Object.entries(this.schema).map(v => v[0]).filter(v => types.find(e => `${e.endpoint.method} ${e.endpoint.uri}` === v) === undefined),
+      shortageEndpoints: shortage,
     }).forEach(v => console.log(v));
-    return new TypedAPIExports(types);
+    return new TypedAPIExports(types, shortage.map(v => parseEndPoint(v)));
   }
 }
 
