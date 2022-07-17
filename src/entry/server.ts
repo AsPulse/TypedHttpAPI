f132c56883643d020936df8cb041079fef32f675
AsPulse <aspulse.x@gmail.com>
2022-07-17 17:04:45 +0900

    Add: overload

--- src/entry/server.ts
+++ src/entry/server.ts
@@ -7,6 +7,7 @@ import type { BetterObjectConstructor } from 'better-object-constructor';
 import { detectDuplicate } from '../util/detectDuplicate';
 import { generateSummary } from '../util/formatSummary';
 import { parseEndPoint } from '../util/parseEndPoint';
+import { TypedHttpAPIImplements } from '../interface/implements';
 
 declare const Object: BetterObjectConstructor;
 
@@ -16,14 +17,26 @@ export class TypedHttpAPIServer<APISchemaType extends APISchema, Raw = undefined
 
   implement<
     EndPoint extends (keyof APISchemaType & APIEndPoint),
-  >(endpoint: EndPoint, processor: APIImplement<APISchemaType, Raw, EndPoint>['processor']) {
-    this.implementations.push({
-      endpoint: parseEndPoint(endpoint),
-      processor,
-      io: this.schema[endpoint],
-    });
+  >(endpoint: EndPoint, processor: APIImplement<APISchemaType, Raw, EndPoint>['processor']): TypedHttpAPIServer<APISchemaType, Raw>
+
+  implement(t: TypedHttpAPIImplements<APISchemaType>): TypedHttpAPIServer<APISchemaType, Raw>
+
+  implement<
+    EndPoint extends (keyof APISchemaType & APIEndPoint),
+  >(a: EndPoint | TypedHttpAPIImplements<APISchemaType, Raw>, b?: APIImplement<APISchemaType, Raw, EndPoint>['processor']) {
+    if(a instanceof TypedHttpAPIImplements) {
+      this.implementations = 
+      [...this.implementations, ...TypedHttpAPIImplements.unpack(a)];
+    } else if (b !== undefined) {
+      this.implementations.push({
+        endpoint: parseEndPoint(a),
+        processor: b,
+        io: this.schema[a],
+      });
+    }
     return this;
   }
+
 
   export(summary = true): TypedAPIExports<Raw> {
     const types: APIExport<Raw>[] = this.implementations.map(v => ({
