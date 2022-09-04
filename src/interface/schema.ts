9ed20e0dde4af1ed9c942e46b9d2d14f6c30624c
AsPulse <aspulse.x@gmail.com>
2022-09-04 10:03:24 +0900

    Add: unwrapped

--- src/interface/schema.ts
+++ src/interface/schema.ts
@@ -2,25 +2,17 @@ import type { InternalRecord } from 'runtypes';
 import type { RuntypeBase } from 'runtypes/lib/runtype';
 import type { BetterObjectConstructor } from 'better-object-constructor';
 import type { HttpRequestMethod } from './httpMethod';
-import type { Record } from 'runtypes';
 
 type FieldRuntypeBase = { [_: string]: RuntypeBase };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AnyAPISchemaIO = APISchemaIO<any, any>;
 
-type UnWrappedAPISchemaIO = {
-  request: Record<FieldRuntypeBase, false>,
-  response: Record<FieldRuntypeBase, false>
-};
-
 type APISchemaIO<T extends FieldRuntypeBase, U extends FieldRuntypeBase> = {
   request: InternalRecord<T, false, false>,
   response: InternalRecord<U, false, false>
 };
 
-type APISchemaIOWrapper<T extends UnWrappedAPISchemaIO> = { [P in keyof UnWrappedAPISchemaIO]: T[P]};
-type APISchemaWrapper<T extends APISchemaTemplate<UnWrappedAPISchemaIO>> = { [P in keyof T & APIEndPoint]: APISchemaIOWrapper<T[P]> };
 
 export type APIEndPoint =`${HttpRequestMethod} /${string}`;
 
@@ -29,11 +21,11 @@ export type APIEndPoint =`${HttpRequestMethod} /${string}`;
  */
 export type APISchema = APISchemaTemplate<AnyAPISchemaIO>;
 
-type APISchemaTemplate<Schema extends AnyAPISchemaIO | UnWrappedAPISchemaIO> = {
+type APISchemaTemplate<Schema extends AnyAPISchemaIO> = {
   [key: APIEndPoint]: Schema,
 };
 
 declare const Object: BetterObjectConstructor;
 
-export const generateAPISchema = <T extends ReadonlyArray<APISchemaTemplate<UnWrappedAPISchemaIO>>>(...input: T): APISchemaWrapper<T[0]> => 
+export const generateAPISchema = <T extends ReadonlyArray<APISchemaTemplate<AnyAPISchemaIO>>>(...input: T): T[0] => 
   Object.fromEntries(Object.entries(input[0]).map(v => [v[0], { request: v[1].request, response: v[1].response }]));
